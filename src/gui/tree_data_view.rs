d88f15d1847d7951950f88f0427a739545a0c6f1
Andrew Newlands <andrew_newlands@hotmail.com>
2025-05-21 17:21:11 +1000

    - tree data view uses fewer elements for highlighting and wraps text properly.

--- src/gui/tree_data_view.rs
+++ src/gui/tree_data_view.rs
@@ -1,5 +1,7 @@
 use super::model::{Highlight, HighlightField};
-use eframe::egui::{self, CentralPanel, Color32, Grid, RichText, ScrollArea, Ui, Vec2};
+use eframe::egui::{
+    self, text::LayoutJob, CentralPanel, Color32, Grid, RichText, ScrollArea, TextFormat, Ui,
+};
 use egui_phosphor::regular as icon;
 
 impl super::App {
@@ -7,63 +9,75 @@ impl super::App {
         CentralPanel::default().show(ctx, |ui| {
             // TODO utilize show_viewport instead to render only visible section
             ScrollArea::vertical().auto_shrink(false).show(ui, |ui| {
-                Grid::new("data").min_col_width(0.0).show(ui, |ui| {
-                    for item in &mut self.gui_data.data {
-                        // Omit rows that should be filtered according to the current regex.
-                        if self.gui_state.filter_state.regex.is_some() && item.highlights.is_empty()
-                        {
-                            continue;
-                        }
-                        if ui
-                            .add(
-                                egui::Button::new(RichText::new(icon::CARET_RIGHT))
-                                    .fill(Color32::TRANSPARENT),
-                            )
-                            .clicked()
-                        {
-                            item.expanded = !item.expanded;
-                        }
-                        ui.label("timestamp");
-                        ui.horizontal(|ui| {
+                Grid::new("data")
+                    .num_columns(3)
+                    .min_col_width(0.0)
+                    .show(ui, |ui| {
+                        for item in &mut self.gui_data.data {
+                            // Omit rows that should be filtered according to the current regex.
+                            if self.gui_state.filter_state.regex.is_some()
+                                && item.highlights.is_empty()
+                            {
+                                continue;
+                            }
+                            let caret = if item.expanded {
+                                icon::CARET_DOWN
+                            } else {
+                                icon::CARET_RIGHT
+                            };
+                            if ui
+                                .add(
+                                    egui::Button::new(RichText::new(caret))
+                                        .fill(Color32::TRANSPARENT),
+                                )
+                                .clicked()
+                            {
+                                item.expanded = !item.expanded;
+                            }
+                            ui.label("timestamp");
                             Self::highlight_text(
                                 ui,
                                 &item.headers,
                                 &item.highlights,
                                 HighlightField::Header,
-                            )
-                        });
-                        //ui.label(item.headers.clone());
-                        ui.end_row();
+                            );
+                            ui.end_row();
 
-                        if item.expanded {
-                            ui.label("");
-                            ui.label("");
-                            //    ui.label(&*item.body);
-                            ui.horizontal(|ui| {
+                            if item.expanded {
+                                ui.label("");
+                                ui.label("");
                                 Self::highlight_text(
                                     ui,
                                     &item.body,
                                     &item.highlights,
                                     HighlightField::Body,
-                                )
-                            });
-                            ui.end_row();
+                                );
+                                ui.end_row();
+                            }
                         }
-                    }
-                });
+                    })
             });
         });
     }
+
     fn highlight_text(
         ui: &mut Ui,
         text: &str,
         highlights: &[Highlight],
         field_specifier: HighlightField,
     ) {
+        let mut job = LayoutJob::default();
         let mut index = 0;
 
-        let style = ui.style_mut();
-        style.spacing.item_spacing = Vec2::new(0.0, 0.0);
+        let text_format_regular = TextFormat {
+            color: Color32::WHITE,
+            ..Default::default()
+        };
+        let text_format_highlighted = TextFormat {
+            color: Color32::BLACK,
+            background: Color32::LIGHT_BLUE,
+            ..Default::default()
+        };
 
         for highlight in highlights {
             if highlight.field != field_specifier {
@@ -71,22 +85,26 @@ impl super::App {
             }
             // Add normal text before the highlight
             if index < highlight.start {
-                ui.label(&text[index..highlight.start]);
+                job.append(
+                    &text[index..highlight.start],
+                    0.0,
+                    text_format_regular.clone(),
+                );
             }
 
             // Add highlighted text
-            ui.label(
-                RichText::new(&text[highlight.start..highlight.end])
-                    .background_color(Color32::LIGHT_BLUE)
-                    .color(Color32::BLACK),
+            job.append(
+                &text[highlight.start..highlight.end],
+                0.0,
+                text_format_highlighted.clone(),
             );
-
             index = highlight.end;
         }
 
         // Add remaining text after the last highlight
         if index < text.len() {
-            ui.label(&text[index..]);
+            job.append(&text[index..], 0.0, text_format_regular.clone());
         }
+        ui.add(egui::Label::new(job).wrap());
     }
 }
